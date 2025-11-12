e808f81d7d2591e3cbf208239f3b620f9f84f7f0
samra <samra@notsoocoool>
2025-11-12 15:29:17 +0530

    created appointment entities

---
+++ z_hospitalSystemSpringPractice/src/main/java/com/hospital/entities/Status.java
@@ -0,0 +1,5 @@
+package com.hospital.entities;
+
+public enum Status {
+	SCHEDULED, CANCELLED
+}
---
+++ z_hospitalSystemSpringPractice/src/main/java/com/hospital/entities/appointment.java
@@ -0,0 +1,41 @@
+package com.hospital.entities;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name = "appointments")
+@Table
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@AttributeOverride(name = "id" , column = @Column(name = "appointment_id"))
+public class appointment extends BaseEntity{
+	
+	@Column(name = "appointment_Date_Time")
+	private LocalDateTime appointmentDateTime;
+	
+	@Enumerated(EnumType.STRING)
+	private Status status;
+	
+	@ManyToOne
+	@JoinColumn(name = "doctor_id")
+	private Doctor doctor;
+	
+	@ManyToOne
+	@JoinColumn(name = "patient_id")
+
+	private Patient patient;
+}
